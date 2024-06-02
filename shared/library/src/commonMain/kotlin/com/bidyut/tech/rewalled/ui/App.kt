aa57af974b4fafaa7a46bbd97892cc84cec03545
Saud Khan <dev@bidyut.com>
2024-06-02 19:02:21 +0800

    Add route argument details

--- shared/library/src/commonMain/kotlin/com/bidyut/tech/rewalled/ui/App.kt
+++ shared/library/src/commonMain/kotlin/com/bidyut/tech/rewalled/ui/App.kt
@@ -5,9 +5,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.bidyut.tech.rewalled.model.Filter
+import com.bidyut.tech.rewalled.model.SubredditFeedId
+import com.bidyut.tech.rewalled.model.WallpaperId
+import com.bidyut.tech.rewalled.model.makeSubredditFeedId
 import com.bidyut.tech.rewalled.ui.screen.SubRedditScreen
 import com.bidyut.tech.rewalled.ui.screen.SubRedditViewModel
 import com.bidyut.tech.rewalled.ui.screen.WallpaperScreen
@@ -32,9 +38,18 @@ fun App() {
         }
         composable(
             route = Route.Wallpaper("{feedId}", "{id}").uri,
+            arguments = listOf(
+                navArgument("feedId") {
+                    type = NavType.StringType
+                    defaultValue = makeSubredditFeedId("EarthPorn", Filter.Rising)
+                },
+                navArgument("id") {
+                    type = NavType.StringType
+                },
+            ),
         ) {
-            val feedId = it.arguments?.getString("feedId").orEmpty()
-            val id = it.arguments?.getString("id").orEmpty()
+            val feedId: SubredditFeedId = it.arguments?.getString("feedId").orEmpty()
+            val id: WallpaperId = it.arguments?.getString("id").orEmpty()
             WallpaperScreen(
                 feedId = feedId,
                 wallpaperId = id,
