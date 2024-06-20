7c3555de2b179ae0a6b7ddf258debed863a42ac6
Saud Khan <dev@bidyut.com>
2024-06-21 00:41:45 +0800

    Add animations to wallpaper screen

--- shared/library/src/commonMain/kotlin/com/bidyut/tech/rewalled/ui/screen/WallpaperScreen.kt
+++ shared/library/src/commonMain/kotlin/com/bidyut/tech/rewalled/ui/screen/WallpaperScreen.kt
@@ -1,5 +1,13 @@
 package com.bidyut.tech.rewalled.ui.screen
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
+import androidx.compose.animation.slideIn
+import androidx.compose.animation.slideOut
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.padding
@@ -17,11 +25,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.unit.center
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.bidyut.tech.rewalled.model.SubredditFeedId
@@ -75,79 +85,96 @@ fun WallpaperScreen(
             )
             Scaffold(
                 bottomBar = {
-                    if (isChromeShown) {
-                        BottomAppBar(
-                            modifier = Modifier.alpha(0.9f),
-                            actions = {
-                                IconButton(
-                                    modifier = Modifier.size(48.dp),
-                                    onClick = {
-                                        navigator.popBackStack()
-                                    },
-                                ) {
-                                    Icon(
-                                        FeatherIcons.ArrowLeft,
-                                        contentDescription = "Back",
-                                    )
-                                }
-                                IconButton(
-                                    modifier = Modifier.size(48.dp),
-                                    onClick = {
-                                        isFullscreen = !isFullscreen
-                                    },
-                                ) {
-                                    if (isFullscreen) {
+                    AnimatedContent(
+                        targetState = isChromeShown,
+                        contentAlignment = Alignment.BottomCenter,
+                        transitionSpec = {
+                            fadeIn() + slideIn { it.center } togetherWith
+                                    fadeOut() + slideOut { it.center }
+                        }
+                    ) { showBottomBar ->
+                        if (showBottomBar) {
+                            BottomAppBar(
+                                modifier = Modifier.alpha(0.9f),
+                                actions = {
+                                    IconButton(
+                                        modifier = Modifier.size(48.dp),
+                                        onClick = {
+                                            navigator.popBackStack()
+                                        },
+                                    ) {
                                         Icon(
-                                            FeatherIcons.Minimize,
-                                            contentDescription = "Fit",
+                                            FeatherIcons.ArrowLeft,
+                                            contentDescription = "Back",
                                         )
-                                    } else {
+                                    }
+                                    IconButton(
+                                        modifier = Modifier.size(48.dp),
+                                        onClick = {
+                                            isFullscreen = !isFullscreen
+                                        },
+                                    ) {
+                                        if (isFullscreen) {
+                                            Icon(
+                                                FeatherIcons.Minimize,
+                                                contentDescription = "Fit",
+                                            )
+                                        } else {
+                                            Icon(
+                                                FeatherIcons.Maximize,
+                                                contentDescription = "Fill",
+                                            )
+                                        }
+                                    }
+                                    IconButton(
+                                        modifier = Modifier.size(48.dp),
+                                        onClick = {
+                                            feed.wallpapers.getOrNull(pagerState.currentPage)
+                                                ?.let { w ->
+                                                    viewModel.coordinator.triggerShareIntent(
+                                                        context,
+                                                        w
+                                                    )
+                                                }
+                                        },
+                                    ) {
                                         Icon(
-                                            FeatherIcons.Maximize,
-                                            contentDescription = "Fill",
+                                            FeatherIcons.Share,
+                                            contentDescription = "Share",
+                                        )
+                                    }
+                                    IconButton(
+                                        modifier = Modifier.size(48.dp),
+                                        onClick = {
+                                            feed.wallpapers.getOrNull(pagerState.currentPage)?.let {
+                                                uriHandler.openUri(it.postUrl)
+                                            }
+                                        },
+                                    ) {
+                                        Icon(
+                                            FeatherIcons.ExternalLink,
+                                            contentDescription = "Open source",
+                                        )
+                                    }
+                                },
+                                floatingActionButton = {
+                                    FloatingActionButton(onClick = {
+                                        feed.wallpapers.getOrNull(pagerState.currentPage)
+                                            ?.let { w ->
+                                                viewModel.coordinator.triggerDownloadIntent(
+                                                    context,
+                                                    w
+                                                )
+                                            }
+                                    }) {
+                                        Icon(
+                                            FeatherIcons.Download,
+                                            contentDescription = "Download",
                                         )
                                     }
                                 }
-                                IconButton(
-                                    modifier = Modifier.size(48.dp),
-                                    onClick = {
-                                        feed.wallpapers.getOrNull(pagerState.currentPage)?.let { w ->
-                                            viewModel.coordinator.triggerShareIntent(context, w)
-                                        }
-                                    },
-                                ) {
-                                    Icon(
-                                        FeatherIcons.Share,
-                                        contentDescription = "Share",
-                                    )
-                                }
-                                IconButton(
-                                    modifier = Modifier.size(48.dp),
-                                    onClick = {
-                                        feed.wallpapers.getOrNull(pagerState.currentPage)?.let {
-                                            uriHandler.openUri(it.postUrl)
-                                        }
-                                    },
-                                ) {
-                                    Icon(
-                                        FeatherIcons.ExternalLink,
-                                        contentDescription = "Open source",
-                                    )
-                                }
-                            },
-                            floatingActionButton = {
-                                FloatingActionButton(onClick = {
-                                    feed.wallpapers.getOrNull(pagerState.currentPage)?.let { w ->
-                                        viewModel.coordinator.triggerDownloadIntent(context, w)
-                                    }
-                                }) {
-                                    Icon(
-                                        FeatherIcons.Download,
-                                        contentDescription = "Download",
-                                    )
-                                }
-                            }
-                        )
+                            )
+                        }
                     }
                 }
             ) { paddingValues ->
@@ -156,22 +183,29 @@ fun WallpaperScreen(
                     key = { feed.wallpapers[it].id }
                 ) { index ->
                     val wallpaper = feed.wallpapers[index]
-                    KamelImage(
-                        resource = asyncPainterResource(wallpaper.getUriForSize(screenWidthPx)),
-                        modifier = modifier.clickable {
-                            isChromeShown = !isChromeShown
-                        }.apply {
-                            if (!isFullscreen) {
-                                padding(paddingValues)
-                            }
-                        },
-                        contentDescription = wallpaper.summary,
-                        contentScale = if (isFullscreen) {
-                            ContentScale.Crop
-                        } else {
-                            ContentScale.Fit
-                        },
-                    )
+                    AnimatedContent(
+                        targetState = isFullscreen,
+                        transitionSpec = {
+                            scaleIn() + fadeIn() togetherWith scaleOut() + fadeOut()
+                        }
+                    ) { isCropped ->
+                        KamelImage(
+                            resource = asyncPainterResource(wallpaper.getUriForSize(screenWidthPx)),
+                            modifier = modifier.clickable {
+                                isChromeShown = !isChromeShown
+                            }.apply {
+                                if (!isCropped) {
+                                    padding(paddingValues)
+                                }
+                            },
+                            contentDescription = wallpaper.summary,
+                            contentScale = if (isCropped) {
+                                ContentScale.Crop
+                            } else {
+                                ContentScale.Fit
+                            },
+                        )
+                    }
                 }
             }
         }
