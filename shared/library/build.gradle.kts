793f93cbbd1aa4ba18b5e1fd6b124b391c189d09
Saud Khan <dev@bidyut.com>
2025-01-04 09:29:45 +0800

    Fix incorrect name for content negotiation library

--- shared/library/build.gradle.kts
+++ shared/library/build.gradle.kts
@@ -36,7 +36,7 @@ kotlin {
             implementation(libs.ktor.client.core)
             implementation(libs.ktor.client.logging)
             implementation(libs.ktor.client.encoding)
-            implementation(libs.ktor.client.contentNavigation)
+            implementation(libs.ktor.client.contentNegotiation)
             implementation(libs.ktor.serialization.json)
 
             implementation(compose.runtime)
