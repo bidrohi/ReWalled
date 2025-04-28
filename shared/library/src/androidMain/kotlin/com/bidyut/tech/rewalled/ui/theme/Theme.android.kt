4759f6306ed0e586f5b52879bd3e48a2e71a0652
Saud Khan <dev@bidyut.com>
2025-04-28 00:05:24 -0400

    Remove deprecated system/navigation bar setColor

--- shared/library/src/androidMain/kotlin/com/bidyut/tech/rewalled/ui/theme/Theme.android.kt
+++ shared/library/src/androidMain/kotlin/com/bidyut/tech/rewalled/ui/theme/Theme.android.kt
@@ -8,11 +8,10 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 
 internal actual fun isDynamicColorSupported(): Boolean {
@@ -36,11 +35,10 @@ internal actual fun configureWindow(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = Color.Transparent.toArgb()
-            window.navigationBarColor = Color.Transparent.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !isInDarkTheme
-            WindowCompat.getInsetsController(window, view).isAppearanceLightNavigationBars =
-                !isInDarkTheme
+            val insetsController = WindowCompat.getInsetsController(window, view)
+            insetsController.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_DEFAULT
+            insetsController.isAppearanceLightStatusBars = !isInDarkTheme
+            insetsController.isAppearanceLightNavigationBars = !isInDarkTheme
         }
     }
 }
