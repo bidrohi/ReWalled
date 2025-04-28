b0658bd5afa7120fb6c275d96667b4c4846b7961
Saud Khan <dev@bidyut.com>
2025-04-27 23:55:25 -0400

    Combine AppActivity and ReWalledActivity

--- app/src/androidMain/kotlin/com/bidyut/tech/rewalled/ReWalledActivity.kt
+++ app/src/androidMain/kotlin/com/bidyut/tech/rewalled/ReWalledActivity.kt
@@ -1,12 +1,21 @@
 package com.bidyut.tech.rewalled
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.bidyut.tech.rewalled.ui.AppActivity
+import androidx.core.view.WindowCompat
+import com.bidyut.tech.rewalled.ui.App
 
-class ReWalledActivity : AppActivity() {
+class ReWalledActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        setContent {
+            App()
+        }
     }
 }
